63d0445e7e6f5c00ea9ea15c0d1a4fb0f1a97035
何冰倩 <hebingqian@cheyipai.com>
2021-04-02 14:34:48 +0800

    最长公共前缀

--- simple/longCommonPre.js
+++ simple/longCommonPre.js
@@ -100,7 +100,8 @@ const longCommonPrefix5 = (strArr) => {
   return getCommonStr(first, last);
 }
 
-
+// 此方法题解命名为分治，用了类似二分查找，查找一个无序数组
+// 个人认为开拓思维可，不乏题解强解做法
 const longCommonPrefix6 = (strArr) => {
   if (!strArr.length) {
     return '';
@@ -114,15 +115,64 @@ const longCommonPrefix6 = (strArr) => {
       console.log('start === end', start, end, arr[start]);
       return arr[start];
     } else {
-
       let middle = Math.floor((end - start)/ 2) + start;
       console.log('middile start end', middle, start, end)
-
       let left = dichotomy(arr, start, middle); 
       let right = dichotomy(arr, middle + 1, end);
+      console.log('left, right, common', left, right, getCommonStr(left, right));
       return getCommonStr(left, right);
     }
   }
   return dichotomy(strArr, 0, strArr.length - 1);
 }
-console.log('6',longCommonPrefix6(["flower","flow","f", "flight", "fl"]))
+// console.log('6',longCommonPrefix6(["flow","fl","flower" ,"flight", "fl","f"]));
+
+
+const longCommonPrefix7 = (strArr) => {
+  if (!strArr.length) {
+    return '';
+  }
+  if (strArr.length === 1) {
+    return strArr[0];
+  }
+  // 获取最短的len
+  let minLen = strArr[0].length;
+  for(let i = 1; i < strArr.length; i ++) {
+    minLen = Math.min(minLen, strArr[i].length);
+  }
+  let minIdx = 0;
+  // 不使用minLen - 1 避免最短长度就是1，或者上面直接条件跳出也可
+  let maxIdx = minLen;
+
+  // 注意这里使用的是substr，截取的是长度不是index，至少是2个数字，middle从1开始
+  while(minIdx < maxIdx) {
+    let middle = Math.floor((maxIdx - minIdx + 1) / 2) + minIdx;
+    if(isCommonPrefix(strArr, middle)) {
+      minIdx = middle;
+    } else {
+      maxIdx = middle - 1;
+    }
+  }
+  return strArr[0].substr(0, minIdx);
+
+  function isCommonPrefix (arr, commonLeft) {
+    const commonStr = arr[0].substr(0, commonLeft);
+    // 不能用includes因为需要按位比较 :P
+    for(let i = 0; i < arr.length; i ++) {
+      for(let j = 0; j < commonStr.length; j ++) {
+        if(arr[i][j] !== commonStr[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
+
+console.log('7',longCommonPrefix7(["ab", "a"]));
+
+/**
+ * 总结
+ * 1、数学思路有提升，稍微不太暴力了
+ * 2、递归、二分查找需要继续学习
+ */
